e9ce6cc277cebe53c4b4abd9029d1d9b2be98c9f
Andrew Paxson <13216842+paxsonsa@users.noreply.github.com>
2024-02-27 14:15:53 -0600

    Add Sticky footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import Header from "@/components/Header";
 
+import Link from "next/link";
+
 export const metadata = {
   title: "🕹️ Andrew Paxson",
   description: "I like to make things for creative people.",
@@ -9,8 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white dark:bg-black text-neutral-800 dark:text-neutral-50 dark:font-light">
-        <main className="flex-row justify-center items-center">{children}</main>
+      <body className="h-screen flex flex-col bg-white dark:bg-black text-neutral-800 dark:text-neutral-50 dark:font-light">
+        <main className="flex-row justify-center items-center mb-auto">
+          {children}
+        </main>
+        <footer className="w-full text-sm p-4 mb-2 flex-row justify-center m-auto text-center text-neutral-400">
+          <p>© 2024 Andrew Paxson</p>
+          <p>This website was made with ♥️ using next.js and tailwindcss.</p>
+          <Link
+            className="font-medium hover:underline"
+            href="https://github.com/paxsonsa/website"
+          >
+            Check out the code here
+          </Link>
+        </footer>
       </body>
     </html>
   );
